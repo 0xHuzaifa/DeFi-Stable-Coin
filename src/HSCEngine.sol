4fd0558e5d36aeb8b394b2a76a461198bb32b586
huzaafi <huzaifasaleem098@gmail.com>
2024-03-09 23:42:16 +0500

    changes

--- src/HSCEngine.sol
+++ src/HSCEngine.sol
@@ -41,7 +41,7 @@ import {AggregatorV3Interface} from "../lib/foundry-chainlink-toolkit/src/interf
  * It is similar to DAI if DAI had no governance, no fees, and was backed by only WETH and WBTC.
  *
  * @notice This contract is the core of the Decentralized Stablecoin system. It handles all the logic
- * for minting and redeeming DSC, as well as depositing and withdrawing collateral.
+ * for minting and redeeming HSC, as well as depositing and withdrawing collateral.
  * @notice This contract is based on the MakerDAO DSS system
  */
 contract HSCEngine is ReentrancyGuard {
@@ -52,14 +52,14 @@ contract HSCEngine is ReentrancyGuard {
     
     error HSCEngine__NeedsMoreThanZero();
     error HSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
-    error HSCEngine__NotAllowedToken();
+    error HSCEngine__NotAllowedToken(address token);
     error HSCEngine__TransferFailed();
     error HSCEngine__BreakHealthFactor(uint256 healthFactorValue);
     error HSCEngine__MintedFailed();
 
     //////////////////////////////
     // State Variables         ///
-    //////////////////////////////
+    //////////////////////////////  
 
     HuzaifaStableCoin private immutable i_hsc;
 
@@ -85,8 +85,8 @@ contract HSCEngine is ReentrancyGuard {
     // Events              //
     /////////////////////////
 
-    event CollateralDeposited(address indexed user, address indexed token, uint256 amount);
-
+    event CollateralDeposited(address indexed user, address indexed token, uint256 indexed amount);
+    event CollateralRedeemed(address indexed user, address indexed token, uint256 indexed amount);
 
     /////////////////////////
     // Modifiers           //
@@ -101,7 +101,7 @@ contract HSCEngine is ReentrancyGuard {
 
     modifier isAllowdToken(address token) {
         if (s_priceFeeds[token] == address(0)) {
-            revert HSCEngine__NotAllowedToken();
+            revert HSCEngine__NotAllowedToken(token);
         }
         _;
     }
@@ -119,23 +119,38 @@ contract HSCEngine is ReentrancyGuard {
             }
             // USD price feed
             for(uint256 i=0; i < tokenAddresses.length; i++) {
-                s_priceFeeds[tokenAddresses[1]] = priceFeedAddresses[i];
+                s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
                 s_collateralTokens.push(tokenAddresses[i]);
             }
             i_hsc = HuzaifaStableCoin(hscAddress);
     }
 
     /////////////////////////////
-    // External Functions     //
-    ////////////////////////////
+    // External Functions     ///
+    /////////////////////////////
 
+    /*
+    * @param tokenCollateralAddress The address of the token to deposit as collateral
+    * @param amountCollateral The amount of collateral to deposit
+    * @param amountHscToMint The amount of Huzaifa Stable Coin to mint
+    * @notice This function will deposit your collateral and mint HSC in one transaction
+    */
+    function depositCollateralAndMintHsc(
+        address tokenCollateralAddress, 
+        address amountCollateral, 
+        uint256 amountHscToMint) external {
+            depostiCollateral(tokenCollateralAddress, amountCollateral);
+            mintHsc(amountHscToMint);
+    }
 
-    function depositCollateralAndMintDsc() external {}
-
+    /*
+     * @param tokenCollateralAddress: The ERC20 token address of the collateral you're depositing
+     * @param amountCollateral: The amount of collateral you're depositing
+     */
     function depostiCollateral(
         address tokenCollateralAddress, 
         uint256 collateralAmount) 
-        external 
+        public 
         moreThanZero(collateralAmount) 
         isAllowdToken(tokenCollateralAddress) 
         nonReentrant {
@@ -148,16 +163,38 @@ contract HSCEngine is ReentrancyGuard {
             }
     } 
 
-    function redeemCollateralForDsc() external {}
+    function redeemCollateralForHsc(address tokenCollateralAddress, uint256 amountCollateral, uint256 amountToBurn) 
+    external moreThanZero(amountCollateral) {
+        burnHsc(amountToBurn);
+        redeemCollateral(tokenCollateralAddress, amountCollateral);
+        // redeemCollateral already checks health factor
+    }
 
-    function redeemCollateral() external {}
+    /*
+     * @param tokenCollateralAddress: The ERC20 token address of the collateral you're redeeming
+     * @param amountCollateral: The amount of collateral you're redeeming
+     * @notice This function will redeem your collateral.
+     * @notice If you have HSC minted, you will not be able to redeem until you burn your HSC
+     */
+    function redeemCollateral(address tokenCollateralAddress, uint256 amountCollateral) 
+    public moreThanZero(amountCollateral) nonReentrant isAllowdToken(tokenCollateralAddress){
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] -= amountCollateral;
+        emit CollateralRedeemed(msg.sender, tokenCollateralAddress, amountCollateral);
+
+        bool success = IERC20(tokenCollateralAddress).transfer(msg.sender, amountCollateral);
+        if (!success) {
+            revert HSCEngine__TransferFailed();
+        }
+
+        _revertIfHealthFactorIsBroken(msg.sender);
+    }
     
     /*
     * @notice follow CEI
     * @param amountHscToMinted The amount of Huzaifa stable coin to minted
     * @notice they must have more collateral value than the minimum threeshold. 
     */
-    function mintDsc(uint256 amountHscToMinted) external moreThanZero(amountHscToMinted) nonReentrant {
+    function mintHsc(uint256 amountHscToMinted) public moreThanZero(amountHscToMinted) nonReentrant {
         s_HSCMinted[msg.sender] += amountHscToMinted;
         // If they minted too much ($150 HSC, $100 ETH)
         _revertIfHealthFactorIsBroken(msg.sender);
@@ -167,10 +204,36 @@ contract HSCEngine is ReentrancyGuard {
         }
     }
 
-    function liquidate() external {}
 
-    function burnDsc() external {}
+    function burnHsc(uint256 amount) public moreThanZero(amount) {
+        s_HSCMinted[msg.sender] -= amount;
 
+        bool success = i_hsc.transferFrom(msg.sender, address(this).balance, amount);
+        if (!success) {
+            revert HSCEngine__TransferFailed();
+        }
+        i_hsc.burn(amount);
+        _revertIfHealthFactorIsBroken(msg.sender); // I don't think this would ever hit
+    }
+
+    /*
+     * @param collateral: The ERC20 token address of the collateral you're using to make the protocol solvent again.
+     * This is collateral that you're going to take from the user who is insolvent.
+     * In return, you have to burn your HSC to pay off their debt, but you don't pay off your own.
+     * @param user: The user who is insolvent. They have to have a _healthFactor below MIN_HEALTH_FACTOR
+     * @param debtToCover: The amount of HSC you want to burn to cover the user's debt.
+     *
+     * @notice: You can partially liquidate a user.
+     * @notice: You will get a 10% LIQUIDATION_BONUS for taking the users funds.
+     * @notice: This function working assumes that the protocol will be roughly 150% overcollateralized in order for this to work.
+     * @notice: A known bug would be if the protocol was only 100% collateralized, we wouldn't be able to liquidate anyone.
+     * For example, if the price of the collateral plummeted before anyone could be liquidated.
+     */
+    function liquidate(address collateral, address user, uint256 debtToCover) 
+    external moreThanZero(debtToCover) nonReentrant {
+        
+    }
+    
     function getHealthFactor() external {}
 
     //////////////////////////////////////////
